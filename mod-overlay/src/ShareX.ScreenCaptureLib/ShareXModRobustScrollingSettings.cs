b5bb7020d80de3277d6d3f2fc8028295bec8caa7
Demorain <whl2638916@126.com>
2026-08-13 21:32:21 +0100

    build: enable v0.3 oversized defaults

--- mod-overlay/src/ShareX.ScreenCaptureLib/ShareXModRobustScrollingSettings.cs
+++ mod-overlay/src/ShareX.ScreenCaptureLib/ShareXModRobustScrollingSettings.cs
@@ -11,17 +11,25 @@ internal sealed class ShareXModRobustScrollingSettings
     public bool Enabled { get; set; }
     public bool ManualStopOnly { get; set; } = true;
     public bool ContinueAfterCombineFailure { get; set; } = true;
-    public int MaxConsecutiveCombineFailures { get; set; } = 8;
+    public int MaxConsecutiveCombineFailures { get; set; } = 0;
     public int MaxConsecutiveUnchangedFrames { get; set; } = 3;
     public bool SaveRawFramesOnFailure { get; set; } = true;
     public int RawFrameHistory { get; set; } = 4;
     public bool DiagnosticsEnabled { get; set; } = true;
     public bool FallbackMatcherEnabled { get; set; } = true;
-    public double FallbackMaxMeanDifference { get; set; } = 14.0;
+    public double FallbackMaxMeanDifference { get; set; } = 20.0;
     public int FallbackMinScrollDelta { get; set; } = 12;
     public double FallbackMaxScrollDeltaRatio { get; set; } = 0.90;
     public int FallbackCoarseStep { get; set; } = 8;
     public string SessionDirectory { get; set; } = "ShareX-Mod\\ScrollingCaptureSessions";
+
+    public bool OversizedCaptureEnabled { get; set; } = true;
+    public int OversizedDimensionThreshold { get; set; } = 60000;
+    public int OversizedPreviewMaxWidth { get; set; } = 1600;
+    public int OversizedPreviewMaxHeight { get; set; } = 16000;
+    public bool OversizedPreferSinglePng { get; set; } = true;
+    public int OversizedFallbackPartHeight { get; set; } = 30000;
+    public string OversizedOutputDirectory { get; set; } = "ShareX-Mod\\OversizedCaptures";
 
     public static ShareXModRobustScrollingSettings Load()
     {
